96f9e0bc9836a13f86ce3c08ecc514007e09d9bc
kevindam96 <kevindam96@gmail.com>
2020-04-18 15:31:18 -0500

    implement and test Mult

--- nand2tetris/projects/04/mult/mult.asm
+++ nand2tetris/projects/04/mult/mult.asm
@@ -7,3 +7,39 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// initialize R2 = 0
+@R2
+M=0
+
+//  for i in range(a):
+//      for i in range(b):
+//          prod = prod + 1
+//  R2 = prod
+
+(LOOP)
+    // if R0 = 0, end loop
+    @R0
+    D=M
+    @END
+    D;JEQ
+    
+    // i = i - 1
+    @R0
+    M=D-1
+
+    // else, R2 = R2 + R1
+    @R2
+    D=M
+    @R1
+    D=D+M
+    @R2
+    M=D
+
+    // restart LOOP
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
