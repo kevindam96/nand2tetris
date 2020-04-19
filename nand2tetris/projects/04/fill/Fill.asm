430a6888571ffaa11ca3bcc47833982a2cebc849
kevindam96 <kevindam96@gmail.com>
2020-04-19 14:34:00 -0500

    implement and test Fill

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -12,3 +12,72 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// Infinite loop
+(LOOP)
+
+    // Initialize index i
+    @i
+    M=0
+        
+    // Probe keyboard
+    @24576
+    D=M
+
+    // If KBD=0 goto @CLEARLOOP
+    @CLEARLOOP
+    D;JEQ
+
+    // Else KBD!=0 goto @FILLLOOP
+    @FILLLOOP
+    0;JMP
+
+    // Loop through screen memory map
+    (CLEARLOOP)
+    // if i==8193 goto LOOP
+    @i
+    D=M
+    @8192
+    D=D-A
+    @LOOP
+    D;JEQ
+
+    // SCREEN[i] = 0
+    @16384
+    D=A
+    @i
+    A=D+M
+    M=0
+
+    // i++
+    @i
+    M=M+1
+
+    // goto CLEARLOOP
+    @CLEARLOOP
+    0;JMP
+
+    // Loop through screen memory map
+    (FILLLOOP)
+    // if (i==8192) goto LOOP
+    @i
+    D=M
+    @8192
+    D=D-A
+    @LOOP
+    D;JEQ
+    
+    // SCREEN[i] = 1
+    @16384
+    D=A
+    @i
+    A=D+M
+    M=-1
+    
+    // i++
+    @i
+    M=M+1
+
+    // goto FILLLOOP
+    @FILLLOOP
+    0;JMP
