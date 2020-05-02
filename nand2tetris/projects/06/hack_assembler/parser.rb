452e335533312f96a7bea626f4fe3f84c99f73cf
kevindam96 <kevindam96@gmail.com>
2020-05-02 15:00:27 -0500

    switch implementation to translate assembly to machine language

--- nand2tetris/projects/06/hack_assembler/parser.rb
+++ nand2tetris/projects/06/hack_assembler/parser.rb
@@ -5,24 +5,23 @@
 # Its purpose is to unpack a Hack assembly language instruction
 # into its underlying fields
 class Parser
-  def initialize
+  def initialize(symbol_table)
     @op_code = ''
+    @symbol_table = symbol_table
     @a_instruction_value = ''
-    @c_instruction_a = ''
-    @c_instruction_c = ''
-    @c_instruction_d = ''
-    @c_instruction_j = ''
-    @symbol_table = initialize_symbol_table
+    @c_instruction_dest = ''
+    @c_instruction_comp = ''
+    @c_instruction_jump = ''
   end
 
-  def initialize_symbol_table
-    symbol_table = { 'R0' => '0', 'R1' => '1', 'R2' => '2', 'R3' => '3',
-                     'R4' => '4', 'R5' => '5', 'R6' => '6', 'R7' => '7',
-                     'R8' => '8', 'R9' => '9', 'R10' => '10', 'R11' => '11',
-                     'R12' => '12', 'R13' => '13', 'R14' => '14',
-                     'R15' => '15' }
-    symbol_table
-  end
+  # def initialize_symbol_table
+  #   symbol_table = { 'R0' => '0', 'R1' => '1', 'R2' => '2', 'R3' => '3',
+  #                    'R4' => '4', 'R5' => '5', 'R6' => '6', 'R7' => '7',
+  #                    'R8' => '8', 'R9' => '9', 'R10' => '10', 'R11' => '11',
+  #                    'R12' => '12', 'R13' => '13', 'R14' => '14',
+  #                    'R15' => '15' }
+  #   symbol_table
+  # end
 
   def parse_instruction(hack_instruction)
     @op_code = hack_instruction[0]
@@ -43,36 +42,47 @@ class Parser
   end
 
   def parse_a_value_from_symbol(symbol)
-    add_symbol_to_table(symbol) unless symbol_to_table.key?(symbol)
     @a_instruction_value = @symbol_table[symbol]
   end
 
-  def add_symbol_to_table(symbol)
-    @symbol_table[symbol] = @symbol_table.size
-  end
-
   def parse_c_instruction(hack_instruction)
-    parse_c_instruction_a(hack_instruction)
-    parse_c_instruction_c(hack_instruction)
-    parse_c_instruction_d(hack_instruction)
-    parse_c_instruction_j(hack_instruction)
+    has_equals = !hack_instruction.index('=').nil?
+    has_semicolon = !hack_instruction.index(';').nil?
+    split_c_instruction(has_equals, has_semicolon, hack_instruction)
   end
 
-  def parse_c_instruction_a(hack_instruction)
-    @c_instruction_a = hack_instruction[3]
+  def split_c_instruction(has_equals, has_semicolon, hack_instruction)
+    if has_equals && has_semicolon
+      parse_c_dest_jump(hack_instruction)
+    elsif has_equals && !has_semicolon
+      parse_c_dest(hack_instruction)
+    elsif !has_equals && has_semicolon
+      parse_c_jump(hack_instruction)
+    else
+      @c_instruction_comp = hack_instruction
+    end
   end
 
-  def parse_c_instruction_c(hack_instruction)
-    @c_instruction_c = hack_instruction[4..9]
+  def parse_c_dest(hack_instruction)
+    equals_split = hack_instruction.split('=')
+    @c_instruction_dest = equals_split[0]
+    @c_instruction_comp = equals_split[1]
   end
 
-  def parse_c_instruction_d(hack_instruction)
-    @c_instruction_d = hack_instruction[10..12]
+  def parse_c_jump
+    semicolon_split = hack_instruction.split(';')
+    @c_instruction_jump = semicolon_split[semicolon_split.size - 1]
+    @c_instruction_comp = semicolon_split[semicolon_split.size - 2]
   end
 
-  def parse_c_instruction_j(hack_instruction)
-    @c_instruction_j = hack_instruction[13..15]
+  def parse_c_dest_jump
+    equals_split = hack_instruction.split('=')
+    equals_semicolon_split = equals_split.split(';')
+    @c_instruction_dest = equals_split[0]
+    @c_instruction_comp = equals_semicolon_split[0]
+    @c_instruction_jump = equals_semicolon_split[1]
   end
+
   ##
   # Add the integer method to the String class
   class String
